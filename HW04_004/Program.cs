77b12e108852e7757fcce3ab258e3c91ed510073
altotass <altotass@gmail.com>
2022-09-01 22:31:01 +0300

    HW4_004

---
+++ HW04_004/Program.cs
@@ -0,0 +1,29 @@
+﻿// необязательная задача 
+// Написать программу сортировки массива от большего к меньшему. 
+// Массив задается размерностью N с клавиатуры, далее заполняется случайными целыми числами.
+
+Console.WriteLine("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] Massiv = new int[n];
+int temp = 0;
+Random r = new Random();
+for(int i = 0; i < Massiv.Length; i++)      // заполнили случайными цифрами массив
+    {
+    Massiv[i] = r.Next(1,100);    //   массив заполняется рандомно из "r"
+    Console.Write($"{Massiv[i]}, ");
+        }
+        for (int i = 0; i < Massiv.Length - 1; i++)
+                for (int j = i + 1; j < Massiv.Length; j++)
+                if (Massiv[i] < Massiv[j]) 
+                {
+                temp = Massiv[i];
+                Massiv[i] = Massiv[j];
+                Massiv[j] = temp;
+                }
+                Console.WriteLine();
+                foreach(int number in Massiv)   // вывод элементов из массива
+        {
+            // Console.Write("отсортированно - ");
+            Console.Write($" " +number);
+    }
+            
