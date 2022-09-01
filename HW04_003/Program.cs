1b597133b3795498e0ece253b4958a71e0ba0f67
altotass <altotass@gmail.com>
2022-09-01 22:19:04 +0300

    HW4_003

---
+++ HW04_003/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void Masss()
+{
+Console.WriteLine("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] Massiv = new int[n];
+for(int i = 0; i < Massiv.Length; i++)      // заполнили случайными цифрами массив
+    {
+        Massiv[i] = new Random().Next(1,100);
+        Console.Write($"{Massiv[i]}, ");
+    }
+}
+Masss();
