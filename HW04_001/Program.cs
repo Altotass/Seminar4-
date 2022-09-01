b827b5fcdf4a0069f18412a24c50c7200d3c3574
altotass <altotass@gmail.com>
2022-09-01 19:35:17 +0300

    HW4_001

---
+++ HW04_001/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double RRR() 
+{
+Console.WriteLine("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В: ");
+int B = Convert.ToInt32(Console.ReadLine());
+double C = Math.Pow(A,B);
+              // Console.WriteLine("А в степени В = " + C);
+return C;
+}
+Console.WriteLine ("А в степени В = " + RRR());
+
